1c83f3c83b7ce8dc14ce16ffd11d55daad9161ee
blogcn <754710343@qq.com>
2017-05-12 08:47:57 +0800

    0

---
+++ 小甲鱼Python视频/python笔记/054translation - 副本.py
@@ -0,0 +1,30 @@
+import urllib
+import urllib.request
+import urllib.parse
+import json
+
+content=input("输入要翻译的内容")
+
+url = 'http://fanyi.youdao.com/translate_o?smartresult=dict&smartresult=rule&sessionFrom=dict2.index'
+data = {}
+data['i'] = content
+data['from']='AUTO'
+data['to'] = 'AUTO'
+data['smartresult']='dict'
+data['client']='fanyideskweb'
+data['salt']='1494517674075'
+data['sign']='69e3c1e4e235fbfcbb783e547a3be32d'
+data['doctype']='json'
+data['version']='2.1'
+data['keyfrom']='fanyi.web'
+data['action']='FY_BY_ENTER'
+data['typoResult']='true'
+
+data = urllib.parse.urlencode(data).encode('utf-8')
+
+response = urllib.request.urlopen(url,data)
+html = response.read().decode('utf-8')
+
+target=json.load(html)
+
+print("翻译结果：%s"%(target['translateResult'][0][0]['tgt']))
