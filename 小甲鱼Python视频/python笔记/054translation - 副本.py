0688b2550970c36bc77ed91aae6d5d80d4cbc18d
blogcn <754710343@qq.com>
2017-05-25 16:43:50 +0800

    1

--- 小甲鱼Python视频/python笔记/054translation - 副本.py
+++ 小甲鱼Python视频/python笔记/054translation - 副本.py
@@ -25,6 +25,6 @@ data = urllib.parse.urlencode(data).encode('utf-8')
 response = urllib.request.urlopen(url,data)
 html = response.read().decode('utf-8')
 
-target=json.load(html)
+target=json.loads(html)
 
-print("翻译结果：%s"%(target['translateResult'][0][0]['tgt']))
+print("翻译结果：%s"  %(target['translateResult'][0][0]['tgt']))
